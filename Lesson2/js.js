e0004e4e753eb6e37ffd8592554a1d305104ead0
Denis <yatesukadam@ukr.net>
2019-04-13 15:42:16 +0300

    2й урок

---
+++ Lesson2/js.js
@@ -0,0 +1,77 @@
+'use strict';
+
+// if (num < 49) {
+//     console.log('Неверно!')
+// } else if (num > 100) {
+//     console.log("Многовыто!")
+// } else {
+//     console.log("Верно!")
+// }
+
+// (num == 50) ? console.log("Верно!") : console.log("Неверно!");
+
+
+// switch (num) {
+//     case num < 49:
+//         console.log('Неверно!');
+//         break;
+//     case num > 100:
+//         console.log("Многовыто!");
+//         break;
+//     case  num > 80:
+//         console.log("Все еще много!");
+//         break;
+//     case 50:
+//         console.log("Верно!");
+//         break;
+//     default:
+//         console.log("Что-то пошло не так!");
+//         break;
+// }   
+
+// let monk = 11
+// switch (monk) {
+//     case 10:
+//         console.log("Bolshe");
+//         break;
+//     case 12:
+//         console.log("Menshe");
+//         break;
+//     case 11:
+//         console.log("Imenno");
+//         break;
+//     default:
+//         console.log("Nenene");
+//         break;
+
+// }(monk < 14) ? console.log("da") : console.log("net");
+
+// let num = 50;
+// // while (num < 55) {
+// //     console.log(num);
+// //     num++;
+
+// do {
+//     console.log(num);
+//     num++;
+
+// }
+// while (num < 55)
+
+// for (let i = 1; i < 8; i++) {
+//     if (i == 6) {
+//         continue
+//     }
+//     console.log(i);
+// }
+
+// for (let mass = 80; mass < 101; mass++) {
+//     if (mass == 90) {
+//         continue;
+//     }
+//     console.log(mass);
+// }
+
+alert( +"Infinity" );
+    
+
