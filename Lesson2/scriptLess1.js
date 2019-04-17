46e1b35c8f247ec44d4a24c5dcc384078c0599c1
Denis <yatesukadam@ukr.net>
2019-04-17 19:11:47 +0300

    Урок 3

--- Lesson2/scriptLess1.js
+++ Lesson2/scriptLess1.js
@@ -1,7 +1,17 @@
 'use strict';
 
-let money = +prompt("Ваш бюджет на месяц?", ""),
+let money, time;
+
+function start() {
+        money = +prompt("Ваш бюджет на месяц?", "");
         time = prompt("Введите дату в формате YYYY-MM-DD", "");
+
+        while(isNaN(money) || money == "" || money == null) {
+                money = +prompt("Ваш бюджет на месяц?", "");
+        }
+}
+start();
+
 
 let appData = {
         budget: money,
@@ -9,49 +19,62 @@ let appData = {
         optionalExpenses: {},
         income: [],
         timeData: time,
-        saving: false
+        saving: true
 };
 
 
-for (let i = 0; i < 2; i++) {
-        let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
-            b = prompt("Во сколько обойдется?", "");
+function chooseExpenses() {
+        for (let i = 0; i < 2; i++) {
+                let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+                    b = prompt("Во сколько обойдется?", "");
+                
+                if (( (typeof(a))=== 'string' && (typeof(a)) != null ) && (typeof(b)) != null 
+                        && a != '' && b != '' && a.length < 50) {
+                        console.log("done");
+                        appData.expenses[a] = b;      
+                } else {
+                        i = i - 1;
+                }
         
-        if (( (typeof(a))=== 'string' && (typeof(a)) != null ) && (typeof(b)) != null 
-                && a != '' && b != '' && a.length < 50) {
-                console.log("done");
-                appData.expenses[a] = b;      
-        } else {
- 
+            
         }
-
-    
-};
-
-appData.moneyPerDay = appData.budget / 30;
-
-alert("Ежедневный бюджет: " + appData.moneyPerDay);
-
-// if (appData.moneyPerDay < 100) {
-//         console.log("Минимальный уровень достатка");
-// } else if (appData.moneyPerDay > 100 && appData.moneyPerDay <2000) {
-//         console.log("Средный уровень достатка");
-// } else if (appData.moneyPerDay > 2000) {
-//         console.log("Высокий уровень достатка");
-// } else {
-//         console.log("Произошла ошибка");
-//  Это первый способ
-switch (appData.moneyPerDay) {
-        case appData.moneyPerDay < 100:
-        console.log("Минимальный уровень достатка");
-        break;
-        case appData.moneyPerDay > 100 && appData.moneyPerDay <2000:
-        console.log("Средный уровень достатка");
-        break;
-        case appData.moneyPerDay > 2000:
-        console.log("Высокий уровень достатка");
-        break;
-        default:
-        console.log("Произошла ошибка");
-        break;
 }
+chooseExpenses();
+
+function detectDayBudget() {
+        appData.moneyPerDay = (appData.budget / 30).toFixed();
+}
+detectDayBudget();
+
+
+function detectLevel() {
+        if (appData.moneyPerDay < 100) {
+                console.log("Минимальный уровень достатка");
+        } else if (appData.moneyPerDay > 100 && appData.moneyPerDay <2000) {
+                console.log("Средный уровень достатка");
+        } else if (appData.moneyPerDay > 2000) {
+                console.log("Высокий уровень достатка");
+        } else {
+                console.log("Произошла ошибка");
+        alert("Ежедневный бюджет: " + appData.moneyPerDay);
+        }
+}
+detectLevel();
+
+function checkSavings() {
+        if (appData.saving == true) {
+                let save = +prompt("Какова сумма накоплений?"),
+                        present = +prompt("Под какой процент?");
+                appData.monthIncome = save/100/12*present;
+                alert("Доход с вашего депозита " + appData.monthIncome) ;
+                }
+}
+
+checkSavings();
+
+function chooseOptExpenses() {
+        appData.optionalExpenses.a1 = prompt("Статья необязательных расходов?"),
+        appData.optionalExpenses.b2 = prompt("Статья необязательных расходов?"),
+        appData.optionalExpenses.c3 = prompt("Статья необязательных расходов?");
+}
+chooseOptExpenses();
