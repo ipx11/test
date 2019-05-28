9007fa1fcdb6445658e5c67a8da6bd758abe05c7
Denis <yatesukadam@ukr.net>
2019-05-28 16:35:07 +0300

    Moduls

---
+++ Lessons19/src/script.js
@@ -0,0 +1,12 @@
+function myModule() {
+    this.hello = function() {
+        return 'Hello';
+    }
+
+    this.goodbye = function() {
+        return 'goodbye';
+    }
+}
+
+module.exports = myModule;
+
